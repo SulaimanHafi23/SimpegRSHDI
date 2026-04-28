77cfbbbbdc2cc3db4e0b32e237e2718a44a796b3
SulaimanHafi <hafif1609@gmail.com>
2026-04-28 21:49:20 +0800

    Translate remaining UI elements to Indonesian - Review button to Periksa

--- resources/views/approvals/leaves/index.blade.php
+++ resources/views/approvals/leaves/index.blade.php
@@ -193,7 +193,7 @@
                                 </td>
                                 <td class="px-4 py-3 text-right">
                                     <a href="{{ route('approvals.leaves.show', $leave->id) }}" class="inline-flex items-center rounded-md bg-blue-50 px-3 py-1.5 text-xs font-semibold text-blue-700 hover:bg-blue-100">
-                                        Review
+                                        Periksa
                                     </a>
                                 </td>
                             </tr>
